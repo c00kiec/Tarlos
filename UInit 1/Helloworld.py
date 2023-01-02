4624c879cffbba46cb773ad34ea3f5c2bcb1ac94
c00kiec <121786481+c00kiec@users.noreply.github.com>
2023-01-02 07:02:52 +0000

    Added Helloworld.py

---
+++ UInit 1/Helloworld.py
@@ -0,0 +1,5 @@
+def harumi():
+    #UR MOM BUYS A BUNCH OF SODA CAUSE IT WAS CHEAP
+    print("hello world hover over your file no you know what leave it like that show your stupidness hover over your file i think you exceeded your ok now stop"))
+if __name__ == "__main__":
+    harumi() 
